35f6537d9cb3c3deb24d316a8f824c23605af4da
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-10-17 14:23:33 -0700

    maintenance lcoal

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { apiRoute} = require('./secrets');
 const app = express();
 app.use(cors());
 
+
+const port = 3000;
+const ip = apiRoute;
+
+// Properties
 app.get("/properties", (req, res) => {
   const itemList = [
     { id: 1, name: "The First Property" },
@@ -13,8 +18,49 @@ app.get("/properties", (req, res) => {
   res.send(itemList);
 });
 
-const port = 3000;
-const ip = apiRoute;
+// Maintenance
+let maintenanceList = [
+  {
+    id: 1,
+    unit: 101,
+    property: "The First Property",
+    propertyId: 1,
+    category: "Plumbing",
+    description: "Leaky faucet in the kitchen",
+    enter: true,
+    fire: false,
+    pet: true,
+    image: "image1.jpg"
+  },
+  {
+    id: 2,
+    unit: 102,
+    property: "The First Property",
+    propertyId: 1,
+    category: "Electrical",
+    description: "Lights not working in the living room",
+    enter: false,
+    fire: true,
+    pet: false,
+    image: "image2.jpg"
+  },
+  {
+    id: 3,
+    unit: 201,
+    property: "The First Property",
+    propertyId: 2,
+    category: "Painting",
+    description: "Wall needs to be repainted in the bedroom",
+    enter: true,
+    fire: false,
+    pet: true,
+    image: "image3.jpg"
+  }
+];
+app.get("/maintenance", (req, res) => {
+  res.send(maintenanceList);
+});
+
 app.listen(port, ip, () => {
   console.log(`Server is running on http://${ip}:${port}`);
 });
