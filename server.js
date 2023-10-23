6ec3682fed456847b2bc6d5bd450e585c06f2ee4
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-10-23 13:43:56 -0700

    Moment and fix accounting call

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { apiRoute} = require('./secrets');
 const bodyParser = require("body-parser");
+const moment = require('moment');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -72,6 +73,44 @@ app.post("/maintenance", (req, res) => {
   maintenanceList.push(newItem);
   res.send(newItem);
 });
+
+// Accounting
+app.get("/accounting/invoices", (req, res) => {
+  const itemList = [
+    { id: 1, date: moment().format('MMMM'), num: 1 },
+    { id: 2, date: moment().subtract(1, 'month').format('MMMM'), num: 2 },
+    { id: 3, date: moment().subtract(2, 'month').format('MMMM'), num: 3 },
+    { id: 1, date: moment().subtract(3, 'month').format('MMMM'), num: 4},
+    { id: 2, date: moment().subtract(4, 'month').format('MMMM'), num: 5},
+    { id: 3, date: moment().subtract(5, 'month').format('MMMM'), num: 6 },
+  ];
+  res.send(itemList);
+});
+
+app.get("/accounting/debit", (req, res) => {
+  const itemList = [
+    { id: 1, date: moment().format('MMMM'), num: 1 },
+    { id: 2, date: moment().subtract(1, 'month').format('MMMM'), num: 2 },
+    { id: 3, date: moment().subtract(2, 'month').format('MMMM'), num: 3 },
+    { id: 1, date: moment().subtract(3, 'month').format('MMMM'), num: 4},
+    { id: 2, date: moment().subtract(4, 'month').format('MMMM'), num: 5},
+    { id: 3, date: moment().subtract(5, 'month').format('MMMM'), num: 6 },
+  ];
+  res.send(itemList);
+});
+
+app.get("/accounting/credit", (req, res) => {
+  const itemList = [
+    { id: 1, date: moment().format('MMMM'), num: 7 },
+    { id: 2, date: moment().subtract(1, 'month').format('MMMM'), num: 2 },
+    { id: 3, date: moment().subtract(2, 'month').format('MMMM'), num: 3 },
+    { id: 1, date: moment().subtract(3, 'month').format('MMMM'), num: 4},
+    { id: 2, date: moment().subtract(4, 'month').format('MMMM'), num: 5},
+    { id: 3, date: moment().subtract(5, 'month').format('MMMM'), num: 6 },
+  ];
+  res.send(itemList);
+});
+
 
 app.listen(port, ip, () => {
   console.log(`Server is running on http://${ip}:${port}`);
